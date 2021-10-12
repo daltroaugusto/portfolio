9ff4db2cc298cd670e7a274de1de79575a402296
Daltro Augusto <daltroaugusto@tutanota.com>
2021-10-12 07:32:00 -0300

    relicensing new meta favicons etc

--- index.script.mjs
+++ index.script.mjs
@@ -1,3 +1,13 @@
+/*  @license
+    Daltro Augusto's Portfolio
+    Copyright (C) 2021  Daltro Augusto
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // @ts-nocheck
 
 import LocaleLoader from "./libs/localeloader/localeloader.js";
@@ -14,6 +24,9 @@ locales
       title: locales.getString("title"),
       description: locales.getString("slogan"),
       author: "Daltro Augusto",
+      locale: locales.locale,
+      url: "https://daltroaugusto.github.io/portfolio/",
+      site_name: locales.getString('title'),
       viewport: "width=device-width, initial-scale=1.0"
     });
   })
--- sites/assistto/js/corrections.js
+++ sites/assistto/js/corrections.js
@@ -1,3 +1,15 @@
+/*  @license
+    Daltro Augusto's Portfolio
+    Copyright (C) 2021  Daltro Augusto
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+// @ts-nocheck
+
 if (window.pagename == "index") {
   let setMapHeight = () => {
     if (window.isMobile.any) {
